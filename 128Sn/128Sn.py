c6fd282f0de6b07e641dc70f863e45c3d10f9120
Jon Dahl <jon@dira.no>
2024-09-05 13:02:23 +0200

    Add NLD plot and print n_levels for each jpi

--- 128Sn/128Sn.py
+++ 128Sn/128Sn.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import kshell_utilities as ksutil
 ksutil.latex_plot()
 ksutil.flags['debug'] = True
@@ -9,13 +10,36 @@ EX_MIN = 4  # Default value. Set this to be larger than the discrete region.
 EX_MAX = 10 # Default value. Set this to the neutron separation energy.
 
 def main():
-    fig, ax = plt.subplots()
-
     res = ksutil.loadtxt(
         path = 'snbg1/200_levels/',
         load_and_save_to_file = True,
     )
-    # res.nld()
+    bins_nld, density, counts = res.nld(
+        return_counts = True,
+        plot = False,
+    )
+
+    j_values = np.unique(res.levels[:, 1])
+
+    print("jp     N levels")
+    for j in j_values:
+        print(f"{j}+    {len(res.levels[np.logical_and(res.levels[:, 1] == j, res.levels[:, 2] == +1)])}")
+        print(f"{j}-    {len(res.levels[np.logical_and(res.levels[:, 1] == j, res.levels[:, 2] == -1)])}")
+
+    fig_1, ax_1 = plt.subplots()
+    ax_1.step(bins_nld, density)
+    ax_1.set_ylabel(r"NLD [MeV$^{-1}$]")
+    ax_1.set_xlabel("E [MeV]")
+    plt.show()
+
+    fig_2, ax_2 = plt.subplots()
+
+    ax_2.step(bins_nld, counts)
+    ax_2.set_ylabel(r"Counts")
+    ax_1.set_xlabel("E [MeV]")
+    plt.show()
+
+    fig_0, ax_0 = plt.subplots()
     bins, gsf_E1, n_transitions, included_transitions = res.gsf(
         bin_width = BIN_WIDTH,
         Ex_min = EX_MIN,
@@ -31,16 +55,16 @@ def main():
         plot = False
     )
     gsf = gsf_E1 + gsf_M1
-    ax.step(bins, gsf, label=r'SM $E1 + M1$', color='grey')
-    ax.step(bins, gsf_M1, label=r'SM $M1$', color='red')
-    ax.step(bins, gsf_E1, label=r'SM $E1$', color='blue')
+    ax_0.step(bins, gsf, label=r'SM $E1 + M1$', color='grey')
+    ax_0.step(bins, gsf_M1, label=r'SM $M1$', color='red')
+    ax_0.step(bins, gsf_E1, label=r'SM $E1$', color='blue')
 
-    ax.set_yscale('log')
-    ax.set_xlabel(r'E$_{\gamma}$ [MeV]')
-    ax.set_ylabel(r'$f(E_{\gamma})$ [MeV$^{-3}$]')
-    # ax.set_ylabel(r'GSF [MeV$^{-3}$]')
-    ax.legend()
-    # fig.savefig('gsf.png', dpi=600)
+    ax_0.set_yscale('log')
+    ax_0.set_xlabel(r'E$_{\gamma}$ [MeV]')
+    ax_0.set_ylabel(r'$f(E_{\gamma})$ [MeV$^{-3}$]')
+    # ax_0.set_ylabel(r'GSF [MeV$^{-3}$]')
+    ax_0.legend()
+    # fig_0.savefig('gsf.png', dpi=600)
     plt.show()
 
 if __name__ == '__main__':
